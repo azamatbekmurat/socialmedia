dbebec17a7fb904d6477693ab27748ddfcefc03d
Azamat Bekmuratov <azamat.bekmuratov24@gmail.com>
2018-06-18 11:24:47 -0700

    add import statements to friendsService

--- src/app/friends.service.ts
+++ src/app/friends.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { User } from './user.model';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Injectable()
 export class FriendsService {
   users: FirebaseListObservable<any[]>;
 
 
-  constructor(private database: AngularFirebase) {
+  constructor(private database: AngularFireDatabase) {
       this.users = database.list('users');
   }
 
--- src/app/user.model.ts
+++ src/app/user.model.ts
@@ -1,4 +1,3 @@
 export class User {
   constructor(public username: string, public email: string, public password: string){}
-
 }
