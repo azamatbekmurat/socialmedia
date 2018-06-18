791e750409fb688e253330b8c2d996b3aafe763f
Rio Atmadja <rioa@uw.edu>
2018-06-18 15:48:36 -0700

    worked on the login page and routing files

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,28 +1,24 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-<<<<<<< HEAD
+import { LoginComponent } from './login/login.component';
+import { AccountComponent } from './account/account.component';
 import { WelcomeComponent} from './welcome/welcome.component';
 
 const appRoutes: Routes = [
   {
     path: '',
     component:WelcomeComponent
+  },
+
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'account',
+    component: AccountComponent
   }
 
-=======
-import { LoginComponent } from './login/login.component';
-import { AccountComponent } from './account/account.component';
-
-const appRoutes: Routes = [
-{
-  path: 'login',
-  component: LoginComponent
-},
-{
-  path: 'account',
-  component: AccountComponent
-}
->>>>>>> b44c8fc7f9f925b4d176b53af13b0c340d8e1151
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
