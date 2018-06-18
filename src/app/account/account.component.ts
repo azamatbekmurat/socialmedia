c38c84544f7210dbf71654f91916e2e336975dab
Azamat Bekmuratov <azamat.bekmuratov24@gmail.com>
2018-06-18 13:49:39 -0700

    fix merge conflicts

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { FirebaseListObservable } from 'angularfire2/database';
+import { Router } from '@angular/router';
+import { FriendsService } from '../friends.service';
+
 
 @Component({
   selector: 'app-account',
