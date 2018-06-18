b44c8fc7f9f925b4d176b53af13b0c340d8e1151
Rio Atmadja <rioa@uw.edu>
2018-06-18 14:06:11 -0700

    added route to the login and component

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,8 +1,17 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AccountComponent } from './account/account.component';
 
 const appRoutes: Routes = [
-
+{
+  path: 'login',
+  component: LoginComponent
+},
+{
+  path: 'account',
+  component: AccountComponent
+}
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
