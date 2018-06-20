57dd6a20d5ed4712d656289ef99453f8b0eb8428
Eva Antipina <eva@>
2018-06-20 08:21:48 -0700

    add updates to user service

--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
 import { User } from './user.model';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AuthenticationService } from './authentication.service';
 
 @Injectable()
 export class UsersService {
   users: FirebaseListObservable<any[]>;
 
 
-  constructor(private database: AngularFireDatabase) {
-    this.users = database.list('userAccounts');
+  constructor(private database: AngularFireDatabase,public afAuth: AuthenticationService) {
+    this.users = database.list('userAccounts/');
   }
 
   getUsers(){
@@ -18,6 +20,25 @@ export class UsersService {
   getUserById(userId: string){
     return this.database.object('userAccounts/' + userId);
   }
+
+  // DoesUserExistInDB(userEmail: string){
+  //   this.users.forEach(user => {
+  //     if (user.email==userEmail) {
+  //       return true;
+  //     }
+  //   })
+  //   return false;
+  // }
+
+  addNewUser(newUser: User) {
+
+   let uid: string = this.afAuth.authState.uid;
+    this.database.list('userAccounts/${uid}/').set(newUser);
+    this.users.push(newUser);
+  }
+
+
+
   //
   // getFriendsOfThisUser(user: User){
   //   var output;
